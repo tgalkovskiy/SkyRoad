6fa0114f867bbfb5aa5764bac9ed2196e5f176e9
timurg <timurg@playtika.com>
2020-10-26 17:18:53 +0300

    asset audio

---
+++ SkyRoad/Assets/Scripts/AssetAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetAudio : AssetPostprocessor
+{
+    private float MaxSize = 200;
+    private float MinSize = 5000;
+    private void OnPreprocessAudio()
+    {
+        var File = new FileInfo(assetPath);
+        var SizeFile = (File.Length)/1024;
+        AudioImporter AudioImporter = (AudioImporter)assetImporter;
+        AudioImporter.loadInBackground = true;
+        AudioImporter.preloadAudioData = true;
+        AudioImporterSampleSettings StandartSetting = AudioImporter.defaultSampleSettings;
+        AudioImporter.defaultSampleSettings = StandartSetting;
+        if (SizeFile < MaxSize)
+        {
+            Debug.Log("Dec");
+            StandartSetting.loadType = AudioClipLoadType.DecompressOnLoad;
+        }
+        else if (SizeFile > MinSize && SizeFile < MaxSize)
+        {
+            Debug.Log("Comp");
+            StandartSetting.loadType = AudioClipLoadType.CompressedInMemory;
+        }
+        else
+        {
+            Debug.Log("Str");
+            StandartSetting.loadType = AudioClipLoadType.Streaming;
+        }
+    }
+}
